7b3f67e77598bd3fa726d238ba0ab8fa033db1dd
v.naghash <vahidngh95@gmail.com>
2025-02-28 20:13:20 +0330

    Initioal Commit

---
+++ android/app/src/main/kotlin/ir/vahidngh/dynamic_form_builder/dynamic_form_builder/MainActivity.kt
@@ -0,0 +1,5 @@
+package ir.vahidngh.dynamic_form_builder.dynamic_form_builder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:dynamic_form_builder/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'Shabnam',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fa', ''),
+        Locale('fa', ''),
+      ],
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
+
